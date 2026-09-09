7e92e4ab783a0311de5ce35ac7527618265dcff6
nzakyahovi <nurul.zakyah@ui.ac.id>
2026-09-09 17:12:13 +0700

    downgrade djago version 5.2

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Nurul Zakyah Ovi",
+        "npm": "2506617582",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan pendidikan."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Ovi",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
